e2084a531f65e3b8b723e7318e3aea63ee26febd
seangol1 <Seangol1@hotmail.com>
2021-07-02 23:30:07 +0100

    fixed login, logout

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db 
+from . import db #
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 
@@ -17,17 +18,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, please try again', category='error')
         else:
             flash('Email does not exist', category='error')
     
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -37,7 +41,6 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        print(password1, password2)
        
         user = User.query.filter_by(email=email).first()
         if user:
@@ -55,8 +58,9 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))               
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created successfully!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
     
